3b0c072969e91c2e6ce01688c49939f925311374
RakinduM <rakindumarambe17@gmail.com>
2024-09-22 20:25:36 +0530

    Few Changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
- import { View, Text } from 'react-native'
- import React from 'react'
-import { Slot } from 'expo-router'
- 
- const RootLayout = () => {
-   return (
-     <Slot/>
-   )
- }
- 
- export default RootLayout
+import { View, Text } from "react-native";
+import React from "react";
+import { Slot, Stack } from "expo-router";
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+};
+
+export default RootLayout;
